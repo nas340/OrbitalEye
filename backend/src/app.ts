7af309866497430131c1780f7eaed53cb3381950
nas340 <niwsa1110@gmail.com>
2025-06-30 15:32:47 +0530

    added UI origin for cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://orbital-eye-1z9n.vercel.app/',
     credentials: true
 }));
 app.use(express.json());
